4a5057024d531aed0e5d113f0b223999fe893f64
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2022-01-14 17:03:47 -0600

    intial

---
+++ src/portfolio.js
@@ -0,0 +1,71 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://rajshekhar26.github.io/cleanfolio',
+  title: 'JS.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Daniel Sosa',
+  role: 'Front End Engineer',
+  description:
+    'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://linkedin.com',
+    github: 'https://github.com',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Project 1',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'Amet asperiores et impedit aliquam consectetur? Voluptates sed a nulla ipsa officia et esse aliquam',
+    stack: ['SASS', 'TypeScript', 'React'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'MongoDB',
+  'React',
+  'Redux',
+  'SASS',
+  'CSS',
+  'Git',
+  'GraphQL',
+  'Jest',
+  'MySQL',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'danielsosa2121@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
